8d8a3baa8a2543f24a36db1b233bf0e574f8b96d
杜娟 <jdu.xian@gmail.com>
2020-05-14 09:33:01 +0800

    add verifyPassword answer

--- src/demo/__test__/register.test.js
+++ src/demo/__test__/register.test.js
@@ -4,9 +4,14 @@ describe("verifyPassword", () => {
   test("should return false when password length < 6", () => {
     // Arrange
     // 设置输入密码password的值为“12345”
+    const testPassword = "12345";
+
     // Act
     // 调用验证密码函数 verifyPassword(password)
+    const result = verifyPassword(testPassword);
+
     // Assert
     // 验证输出结果是否为false
+    expect(result).toEqual(false);
   });
 });
